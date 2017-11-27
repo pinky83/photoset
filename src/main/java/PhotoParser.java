9f82414dfa3c71db104ae82bc8b9b1c153710267
Dmitriy Yakovenko <dmitriy.yakovenko@ionidea.com>
2017-11-27 14:34:48 +0200

    initial commit

---
+++ src/main/java/PhotoParser.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Dmitry Yakovenko on 11/27/2017.
+ *
+ */
+public class PhotoParser {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ test/PhotoParserTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Dmitriy Yakovenko on 11/27/2017.
+ *
+ */
+public class PhotoParserTest {
+    @Test
+    public void testOne () {
+        System.out.println("test test");
+    }
+}
